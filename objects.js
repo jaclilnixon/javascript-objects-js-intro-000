f94629f0492888922d39060ec1c1d4a131c6ab98
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-08 07:32:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,6 @@ var playlist = { artistName: "songTitle" };
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist = { artist: artistName, song: songTitle }
-};
+playlist[artistName] = songTitle;
+return playlist
+}
